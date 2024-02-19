f5b40eab7f299a6ed5d0a827700d69b990bd0e74
Norbert Śleziak <norr.bitt@gmail.com>
2024-02-19 19:37:59 +0100

    Add valid checks to services

--- backend/src/main/java/pl/norbit/backend/service/LicenseService.java
+++ backend/src/main/java/pl/norbit/backend/service/LicenseService.java
@@ -1,59 +1,91 @@
 package pl.norbit.backend.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.norbit.backend.exception.NotValidLicenseException;
+import pl.norbit.backend.exception.RequestException;
 import pl.norbit.backend.model.License;
+import pl.norbit.backend.model.LicenseType;
 import pl.norbit.backend.repository.LicenseRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class LicenseService {
-    private final LicenseRepository licenseRepository;
 
-    public LicenseService(LicenseRepository licenseRepository) {
-        this.licenseRepository = licenseRepository;
-    }
+    private final LicenseRepository licenseRepository;
 
     public License save(License license) {
         long now = System.currentTimeMillis();
 
+        if(license.getOwner() == null) throw new RequestException("Owner cannot be null");
+
+        int daysToExpire = license.getDaysToExpire();
+
+        if(daysToExpire != 0){
+            license.setExpirationDate(now + 1000L * 60 * 60 * 24 * daysToExpire);
+            license.setLicenseType(LicenseType.DEFAULT);
+        }else{
+            license.setLicenseType(LicenseType.LIFETIME);
+        }
+
         license.setCreationDate(now);
-        license.setExpirationDate(now + 1000L * 60 * 60 * 24 * license.getDaysToExpire());
-        license.setKey(UUID.randomUUID().toString());
+        license.setLicenseKey(UUID.randomUUID().toString());
 
         return licenseRepository.save(license);
     }
 
-    public void updateActive(String key){
-        License license = findByKey(key);
-        license.setLastActive(System.currentTimeMillis());
-        licenseRepository.save(license);
+    public void updateActive(License license){
+        String licenseKey = license.getLicenseKey();
+        String serverKey = license.getServerKey();
+
+        if(licenseKey == null) throw new RequestException("License key cannot be null");
+
+        if(serverKey == null) throw new RequestException("Server key cannot be null");
+
+        License repoLicense = findByKey(licenseKey);
+
+        if(repoLicense == null) throw new NotValidLicenseException("License not found");
+
+        repoLicense.setLastActive(System.currentTimeMillis());
+
+        licenseRepository.save(repoLicense);
     }
 
-    public boolean canStart(String key){
+    public void isValid(String key){
         License license = findByKey(key);
 
-        if(license == null) return false;
+        if(license == null) throw new NotValidLicenseException("License not found");
 
-        if(isExpired(license)) return false;
+        if(isExpired(license)) throw new NotValidLicenseException("License is expired");
 
-        if(license.getLastActive() == 0) return true;
+        if(license.getLastActive() == 0) return;
 
+        if(isLastActive(license)) throw new NotValidLicenseException("License was last active less than 2 minutes ago");
+    }
+
+    private boolean isLastActive(License license){
         long time = 1000 * 60 * 2;
 
-        return System.currentTimeMillis() - license.getLastActive() > time;
+        return license.getLastActive() + time > System.currentTimeMillis();
     }
 
     private boolean isExpired(License license){
+        if(license.getLicenseType() == LicenseType.LIFETIME) return false;
+
         return license.getExpirationDate() < System.currentTimeMillis();
     }
 
     public License findByKey(String key) {
-        return licenseRepository.findByKey(key).orElse(null);
+        return licenseRepository.findByLicenseKey(key).orElse(null);
     }
 
-    public void deleteById(String id) {
+    public void deleteById(Long id) {
+        licenseRepository.findById(id)
+                .orElseThrow(() -> new RequestException("Token with id " + id + " does not exist!"));
+
         licenseRepository.deleteById(id);
     }
 
--- backend/src/main/java/pl/norbit/backend/service/TokenService.java
+++ backend/src/main/java/pl/norbit/backend/service/TokenService.java
@@ -1,7 +1,9 @@
 package pl.norbit.backend.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.norbit.backend.exception.LastTokenException;
+import pl.norbit.backend.exception.NotValidTokenException;
 import pl.norbit.backend.exception.RequestException;
 import pl.norbit.backend.model.Token;
 import pl.norbit.backend.model.TokenType;
@@ -9,42 +11,59 @@ import pl.norbit.backend.repository.TokenRepository;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class TokenService {
 
     private final TokenRepository tokenRepository;
 
-    public TokenService(TokenRepository tokenRepository) {
-        this.tokenRepository = tokenRepository;
-    }
-
     public List<Token> findByTokenType(TokenType tokenType) {
-        return tokenRepository.findTokensByTokenType(tokenType.name());
+        return tokenRepository.findTokensByTokenType(tokenType);
     }
 
-    public void save(Token token) {
-        String tokenTypeString = token.getTokenType();
+    public void verifyToken(String token, TokenType expectedTokenType) {
+        Token tokenEntity = tokenRepository.findTokenByAccessToken(token)
+                .orElseThrow(() -> new NotValidTokenException("Wrong token!"));
 
-        if(tokenTypeString  == null) throw new RequestException("Token type cannot be null");
+        TokenType tokenType = tokenEntity.getTokenType();
+
+        if(tokenType == TokenType.ADMIN) return;
+
+        if (tokenType != expectedTokenType) {
+            throw new NotValidTokenException(("Token type is not valid! Expected: " + expectedTokenType.name()));
+        }
+    }
+    public Token create(String type) {
+        if(type == null) throw new RequestException("Token request cannot be null!");
+
+        Token token = new Token();
+
+        TokenType tokenType;
 
         try {
-            TokenType.valueOf(tokenTypeString.toUpperCase());
+            tokenType = TokenType.valueOf(type.toUpperCase());
         }
         catch (IllegalArgumentException e){
-            String message = "Token type " + tokenTypeString +" is not valid! Valid values: " + Arrays.toString(TokenType.values());
+            String message = "Token type " + type  +" is not valid! Valid values: " + Arrays.toString(TokenType.values());
 
             throw new RequestException(message);
         }
+        token.setCreationDate(System.currentTimeMillis());
+        token.setAccessToken(UUID.randomUUID().toString());
+        token.setTokenType(tokenType);
 
         tokenRepository.save(token);
+
+        return token;
     }
 
-    public void deleteById(String id) {
+    public void deleteById(Long id) {
         Token token = tokenRepository.findById(id)
                 .orElseThrow(() -> new RequestException("Token with id " + id + " does not exist!"));
 
-        if(token.getTokenType().equals(TokenType.ADMIN.name())){
+        if(token.getTokenType() == TokenType.ADMIN){
             List<Token> adminTokens = findByTokenType(TokenType.ADMIN);
 
             if(adminTokens.size() == 1) throw new LastTokenException("You cannot delete the last ADMIN token!");
