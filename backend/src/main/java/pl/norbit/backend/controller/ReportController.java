691a58bd05e473b23d0e8cbbcf498e08a65cf06e
Norbert Śleziak <norr.bitt@gmail.com>
2024-02-26 01:34:02 +0100

    Create ReportController.java

---
+++ backend/src/main/java/pl/norbit/backend/controller/ReportController.java
@@ -0,0 +1,36 @@
+package pl.norbit.backend.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.norbit.backend.model.token.TokenType;
+import pl.norbit.backend.service.ReportService;
+import pl.norbit.backend.service.TokenService;
+
+import static pl.norbit.backend.controller.ReportController.Routes.GENERATE;
+import static pl.norbit.backend.controller.ReportController.Routes.ROOT;
+
+@RestController
+@RequestMapping(path = ROOT)
+@AllArgsConstructor
+public class ReportController {
+    private final TokenService tokenService;
+    private final ReportService reportService;
+
+    @GetMapping(path = GENERATE)
+    public ResponseEntity<byte[]> generate(@RequestHeader("token") String token){
+        tokenService.verifyToken(token, TokenType.ADMIN);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"report.xlsx\"")
+                .body(reportService.getReportFile());
+    }
+    static final class Routes {
+        static final String ROOT = "/api/v1/report";
+        static final String GENERATE = "/generate/excel";
+    }
+}
