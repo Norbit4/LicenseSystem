d8a01d8b730c25ddd64dead26bcc165065077e60
Norbert Śleziak <norr.bitt@gmail.com>
2024-02-28 22:28:39 +0100

    Add Dockerfile to discord bot

--- discordbot/src/service/requestservice.js
+++ discordbot/src/service/requestservice.js
@@ -1,6 +1,6 @@
 const axios= require('axios');
 
-axios.defaults.baseURL = 'http://localhost:8080/api/v1';
+axios.defaults.baseURL = 'http://backend:8080/api/v1';
 axios.defaults.headers.common['token'] = 'admin-secret-token';
 
 const testValue = process.env.test;
