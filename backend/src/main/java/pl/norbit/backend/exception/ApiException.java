33974c8f7c63b99149de92685b6f03d1c77529dc
Norbert Śleziak <norr.bitt@gmail.com>
2024-03-09 15:06:50 +0100

    Clear code

--- backend/src/main/java/pl/norbit/backend/exception/ApiException.java
+++ backend/src/main/java/pl/norbit/backend/exception/ApiException.java
@@ -4,5 +4,7 @@ import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-public record ApiException(String message, HttpStatus status, ZonedDateTime timestamp) {
+public record ApiException(String message,
+                           HttpStatus status,
+                           ZonedDateTime timestamp) {
 }
